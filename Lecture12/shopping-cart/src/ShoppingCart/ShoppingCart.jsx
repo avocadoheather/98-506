6176ec3e6278926d099f5e15d929e5b6484ad492
Heather Brayer <hp.brayer@gmail.com>
2025-11-18 20:43:40 -0500

    Changes for Lecture12

--- Lecture12/shopping-cart/src/ShoppingCart/ShoppingCart.jsx
+++ Lecture12/shopping-cart/src/ShoppingCart/ShoppingCart.jsx
@@ -11,7 +11,11 @@
  */
 export default function ShoppingCart({shoppingCart}) {
     // Calculate the total cost of our shopping cart, and place it in total
+    const total = shoppingCart.reduce((acc, current) => acc + current.cost * current.number, 0);
+        // acc -> accumulator; second argument 0 is starting value of acc
     
     
-    return JSON.stringify(shoppingCart);
+    //return JSON.stringify(shoppingCart);
+    return `$${total}`;
+    // return shoppingCart.map(e => <div>{e.name}</div>)
 }
